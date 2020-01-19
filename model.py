ae4fbb0ac454808c6c5440e64ec2203ffc0abedf
cyberpunk317 <44346535+cyberpunk317@users.noreply.github.com>
2020-01-20 00:38:31 +0200

    Implemented architecture

--- model.py
+++ model.py
@@ -3,11 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ToiPool():
-    def __init__(self):
-        pass
+class ToiPool(nn.Module):
+    def __init__(self, frames, height, width):
+        super(ToiPool, self).__init__()
+        self.D = frames
+        self.height = height
+        self.width = width
 
-    
+
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
@@ -24,19 +27,34 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.conv1 = nn.Conv3d(input_size, 64, (3, 3, 3))
-        self.pool1 = nn.MaxPool3d((1,2,2))
+        self.pool1 = nn.MaxPool3d((1, 2, 2))
         self.conv2 = nn.Conv3d(64, 128, (3, 3, 3))
-        self.conv3 = nn.Conv3d(128, 256, (3, 3, 3))
-        self.conv4 = nn.Conv3d(256, 512, (3, 3, 3))
-        self.conv5 = nn.Conv3d(512, 512, (3, 3, 3))
-        self.toi2 = ToiPool(512, (8, 8, 8))
-        self.toi5 = ToiPool(128, (8, 8, 8))
-        self.conv11 = nn.Conv1d(512)
+        self.pool2 = nn.MaxPool3d((2, 2, 2))
+        self.conv3a = nn.Conv3d(128, 256, (3, 3, 3))
+        self.conv3b = nn.Conv3d(256, 256, (3, 3, 3))
+        self.pool3 = nn.MaxPool3d((2, 2, 2))
+        self.conv4a = nn.Conv3d(256, 512, (3, 3, 3))
+        self.conv4b = nn.Conv3d(512, 512, (3, 3, 3))
+        self.pool4 = nn.MaxPool3d((2, 2, 2))
+        self.conv5a = nn.Conv3d(512, 512, (3, 3, 3))
+        self.conv5b = nn.Conv3d(512, 512, (3, 3, 3))
+        self.toi2 = ToiPool(8, 8, 8)
+        self.toi5 = ToiPool(1, 4, 4)
+        self.conv11 = nn.Conv1d(512, 8192, 1)
         self.fc6 = nn.Linear(fc6_units, fc7_units)
         self.fc7 = nn.Linear(fc7_units, fc7_units)
 
-    def forward(self, state):
+    def forward(self, frame):
         """Build a network that maps state -> action values."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        x = self.pool1(F.leaky_relu(self.conv1(frame)))
+        x = self.pool2(F.leaky_relu(self.conv2(x)))
+        x = self.pool3(F.leaky_relu(
+            self.conv3b(F.leaky_relu(self.conv3a(x)))))
+        x = self.pool4(F.leaky_relu(
+            self.conv4b(F.leaky_relu(self.conv4a(x)))))
+        x = self.toi2(F.leaky_relu(
+            self.conv5b(F.leaky_relu(self.conv5a(x)))))
+        x = self.toi5(x)
+        x = self.conv11(x)
+        x = self.fc6(x)
+        return self.fc7(x)
