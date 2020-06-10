a035009e4d28482314aad6967b751cdf869c86ab
cyberpunk317 <cyberpank317@gmail.com>
2020-06-10 23:59:21 +0300

    Visualize predictions

--- utils.py
+++ utils.py
@@ -34,3 +34,32 @@ def plot_transformed_data_sample(dataset, idx, transform):
     ax.add_patch(box)
     plt.title("Transformed sample image")
     plt.show()
+    
+    
+def visualize_model(model, dataloaders, class_names, num_images=10):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    was_training = model.training
+    model.to(device)
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, batch in enumerate(dataloaders['val']):
+            inputs = batch['image'].to(device, dtype=torch.float)
+            labels = batch['label'].to(device, dtype=torch.int64)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            
+            fig, axs = plt.subplots(2,5)
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                axs[j//5, j%5].axis('off')
+                axs[j//5, j%5].set_title('predicted: {}'.format(class_names[preds[j]]))
+                imshow(inputs.cpu().data[j], axs[j//5, j%5])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
